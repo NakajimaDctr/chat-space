8eabe055cfa80d252e1f10a5720e3cd7d1e9edd4
NakajimaDctr <drcardhms@gmail.com>
2020-02-05 15:32:25 +0900

    add message function

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,37 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  has_many :messages
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    # グループに紐づくメッセージから最新のメッセージを取得
+    last_message = messages.order("created_at DESC").first
+
+    # 最新のメッセージが存在する場合
+    if last_message.present?
+
+      # 最新のメッセージがテキストの場合
+      if last_message.content?
+        # テキストの内容を返す
+        last_message.content
+      
+      # 最新のメッセージが画像の場合
+      else
+        '画像が投稿されています'
+      end
+    
+    # 最新のメッセージが存在しない場合
+    else
+      'まだメッセージはありません'
+    end
+  end
+  def show_member
+    members_array = []
+    users.each do |user|
+      members_array << user.name
+    end
+    members = members_array.join(", ")
+  end
+
 end
